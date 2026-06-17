1a4e2c04864110a879565a98eb8193d3a7049dda
Akshaya A <auakshaya006@gmail.com>
2026-06-17 12:04:41 +0530

    Update notifications.js

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,14 @@
-export function fetchNotifications() {
+export async function fetchNotifications() {
+  try {
+    const response = await fetch("http://localhost:3000/notifications");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch notifications");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
